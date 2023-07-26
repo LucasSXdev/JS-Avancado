3d8ad986a699511742913640d88c53f48c03af66
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-07-26 05:55:49 -0300

    adicionando aula 16

---
+++ 16-utilizando-await/index.js
@@ -0,0 +1,46 @@
+/*Um outro recurso muito útil das funções async é o await.
+ Ele é uma palavra reservada do javascript que nos permite 
+ esperar pela execução de uma promise dentro de uma função async,
+ ou seja, se nossa função async depende de uma outra promise, podemos
+ esperar pela sua execução de forma “limpa” sem utilizar o .then().
+ Para usar o await basta colocá-lo antes da chamada da função assíncrona:*/
+
+//quando utilizamos o await ele vai esperar a promise ser resolvida
+//todo o código abaixo do await será bloqueado até que o await seja resolvido
+//só podemos utilizar o await dentro de uma função async, em funções normais não funcionam
+
+async function asyncSum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return Promise.reject("arguments must be of type number");
+  }
+  return a + b;
+}
+
+async function execute() {
+  const sumResult = await asyncSum(50, 33);
+  console.log(sumResult);
+}
+
+//E nós ainda podemos tratar as rejeições dentro de um bloco try catch convencional:
+
+// ...
+
+async function asyncSubtract(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return Promise.reject("arguments must be of type number");
+  }
+  return a - b;
+}
+
+async function execute() {
+  try {
+    const sumResult = await asyncSum(50, 33);
+    console.log(sumResult);
+    const subtractResult = await asyncSubtract(50, null);
+    console.log(subtractResult);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+execute();
