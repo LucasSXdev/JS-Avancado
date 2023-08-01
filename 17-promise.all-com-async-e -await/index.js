d32412e0cc425e364123891e162504e791a7c086
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-08-01 04:14:17 -0300

    adicionando alterações na aula 18

---
+++ 17-promise.all-com-async-e -await/index.js
@@ -0,0 +1,13 @@
+function waitFor(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return;
+    }, seconds * 1000);
+  });
+}
+
+const numbers = [4, 5, 9, 13, 77];
+const squares = numbers.map(async (number) => {
+  await waitFor(2);
+  return number * number;
+});
--- 18-refatorando-promises/index.js
+++ 18-refatorando-promises/index.js
@@ -1,23 +1,28 @@
 async function imc(weight, height) {
-  if (typeof weight !== "number" || typeof height !== "number") {
-    return Promise.reject("arguments must be of type number");
+  if (typeof weight != "number" || typeof height != "number") {
+    return Promise.reject("arguments must be a type number");
   }
-  return weight / (height * height);
+  weight / height ** 2;
 }
 
 async function showImc(weight, height) {
   try {
-    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`);
-
+    console.log("calculando...");
     const result = await imc(weight, height);
 
-    console.log(`O resultado do IMC foi de ${result}.`);
+    console.log(`o resultado do IMC foi ${result}`);
 
-    if (result < 18.5) console.log("Situação: MAGREZA");
-    else if (result < 25) console.log("Situação: NORMAL");
-    else if (result < 30) console.log("Situação: SOBREPESO");
-    else if (result < 40) console.log("Situação: OBESIDADE");
-    else console.log("Situação: OBESIDADE GRAVE");
+    if (result <= 18.5) {
+      console.log("magreza");
+    } else if (result <= 24.9) {
+      console.log("normal");
+    } else if (result <= 29.9) {
+      console.log("sobrepeso");
+    } else if (result <= 39.9) {
+      console.log("obesidade");
+    } else {
+      console.log("obesidade grave");
+    }
   } catch (error) {
     console.log(error);
   }
